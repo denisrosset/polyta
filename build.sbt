268496787586b861d0776d0ee3bef0848906de37
Denis Rosset <physics@denisrosset.com>
2015-03-09 18:37:17 +0100

    Moved to latest Spire.

--- build.sbt
+++ build.sbt
@@ -2,9 +2,9 @@ name := "Polyta"
 
 organization := "com.faacets"
 
-version := "0.1.0-SNAPSHOT"
+version := "0.9.1-SNAPSHOT"
 
-scalaVersion := "2.11.4"
+scalaVersion := "2.11.5"
 
 resolvers ++= Seq(
   "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
@@ -14,9 +14,9 @@ resolvers ++= Seq(
 libraryDependencies ++= Seq(
   "org.scalacheck" %% "scalacheck" % "1.11.6" % "test",
   "org.scalatest" % "scalatest_2.10" % "2.2.1" % "test",
-  "org.spire-math" %% "spire" % "0.9.1-SNAPSHOT",
-  "net.alasc" %% "alasc" % "0.1.0-SNAPSHOT",
-  "com.faacets" %% "qalg" % "0.1.0-SNAPSHOT",
+  "org.spire-math" %% "spire" % "0.9.1",
+  "net.alasc" %% "alasc" % "0.9.1-SNAPSHOT",
+  "com.faacets" %% "qalg" % "0.9.1-SNAPSHOT",
   "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2"
 )
 
--- src/main/scala/polyta/package.scala
+++ src/main/scala/polyta/package.scala
@@ -1,8 +1,9 @@
 package com.faacets
 
 import spire.algebra._
+import spire.algebra.partial._
 import spire.math.Rational
-import spire.util.Nullbox
+import spire.util.Opt
 
 import qalg.immutable.QVector
 import net.alasc.algebra._
@@ -14,7 +15,7 @@ package object polyta {
     def toIndexedSeq(qv: QVector): IndexedSeq[Rational] = qv.toIndexedSeq
   }
 
-  final class QVectorPermutationAction[P: FiniteGroup: FaithfulPermutationAction] extends NullboxPartialAction[QVector, P]{
+  final class QVectorPermutationAction[P: FiniteGroup: FaithfulPermutationAction] extends PartialAction[QVector, P]{
     import net.alasc.syntax.permutationAction._
     import spire.syntax.group._
     import spire.syntax.action._
@@ -22,11 +23,11 @@ package object polyta {
     override def actlIsDefined(p: P, qv: QVector) = p.supportMax.getOrElseFast(-1) < qv.length
     override def actrIsDefined(qv: QVector, p: P) = p.supportMax.getOrElseFast(-1) < qv.length
 
-    def partialActl(p: P, qv: QVector): Nullbox[QVector] =
-      if (p.supportMax.getOrElseFast(-1) >= qv.length) Nullbox.empty[QVector] else
-        Nullbox(QVector.tabulate(qv.length)( i => qv(i <|+| p) ))
+    def partialActl(p: P, qv: QVector): Opt[QVector] =
+      if (p.supportMax.getOrElseFast(-1) >= qv.length) Opt.empty[QVector] else
+        Opt(QVector.tabulate(qv.length)( i => qv(i <|+| p) ))
 
-    def partialActr(qv: QVector, p: P): Nullbox[QVector] = partialActl(p.inverse, qv)
+    def partialActr(qv: QVector, p: P): Opt[QVector] = partialActl(p.inverse, qv)
   }
-  implicit def QVectorPermutationAction[P: FiniteGroup: FaithfulPermutationAction]: NullboxPartialAction[QVector, P] = new QVectorPermutationAction[P]
+  implicit def QVectorPermutationAction[P: FiniteGroup: FaithfulPermutationAction]: PartialAction[QVector, P] = new QVectorPermutationAction[P]
 }
