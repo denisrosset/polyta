6e07046c3a798343c2fb0384edee5542cd05346f
Denis Rosset <physics@denisrosset.com>
2016-04-09 18:39:54 +0800

    Restored QSopt.

--- src/main/scala/polyta/ApproxOrder.scala
+++
@@ -1,8 +0,0 @@
-package com.faacets
-package polyta
-
-import scala.{specialized => sp}
-
-import spire.algebra._
-
-trait ApproxOrder[@sp(Double) A] extends PartialOrder[A]
--- src/main/scala/polyta/solvers/QsoptEx.scala
+++ src/main/scala/polyta/solvers/QsoptEx.scala
@@ -13,34 +13,35 @@ import spire.std.tuples._
 import spire.syntax.group._
 import spire.util.Opt
 
-import qalg.algebra._
-import qalg.algos._
-
 import net.alasc.algebra._
-import net.alasc.math.{Grp, Perm}
 import net.alasc.std.any._
-import net.alasc.syntax.shiftablePermutation._
 
 import formats._
 import formats.qsopt._
 
 import sys.process._
 
-case class LinearSolution[V](status: SolverStatus[String], fval: Rational, xopt: V)
+import scalin.{Mat, Vec}
+import scalin.immutable.dense._
+import scalin.syntax.all._
+
+case class LinearSolution(status: SolverStatus[String], fval: Rational, xopt: Vec[Rational])
+
 object QsoptEx {
-  def solve[V, G](hPolytope: HPolytope[V, Rational], direction: Direction, f: V, integerVariables: Set[Int])(implicit pack: PackField.ForV[V, Rational]): LinearSolution[V] = {
+
+  def solve(hPolytope: HPolytopeM[Rational], direction: Direction, f: Vec[Rational], integerVariables: Set[Int]): LinearSolution = {
     val input = File.createTempFile("solvelp", ".lp")
     val output = File.createTempFile("solvelp", ".sol")
     val writer = new PrintWriter(input)
     val ineqs = hPolytope.allFacets.zipWithIndex.map { case (facet, i) => LPConstraint("ineq" + (i + 1).toString, facet.inequality) }
     val eqs = hPolytope.equalities.zipWithIndex.map { case (equality, i) => LPConstraint("eq" + (i + 1).toString, equality) }
-    val variableNames = Format.x1toN(hPolytope.nX)
+    val variableNames = Format.x1toN(hPolytope.dim)
     val lpData = LPData("linearProblem",
       variableNames,
       LPObjective("obj", direction, f),
       ineqs ++ eqs,
       integerVariables)
-    FormatWrite[LPData[V]].write(lpData, writer)
+    FormatWrite[LPData].write(lpData, writer)
     writer.close
     val out = new StringBuilder
     val err = new StringBuilder
@@ -54,7 +55,8 @@ object QsoptEx {
     val reader = new FileReader(output)
     val data = FormatRead[SolData].parse(reader).get
     reader.close
-    LinearSolution[V](data.status, data.optimalValue.getOrElse(0),
-      VecBuild[V, Rational].build(variableNames.map(data.variables.getOrElse(_, Rational.zero)):_*))
+    LinearSolution(data.status, data.optimalValue.getOrElse(0),
+      vec(variableNames.map(data.variables.getOrElse(_, Rational.zero)):_*))
   }
+
 }
